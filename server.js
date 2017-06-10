ae3c2688662d8d9ef4367989847cb929dbe8d3d5
Aruk Pupatee <aruk.pupatee@gmail.com>
2017-06-10 17:00:07 +0700

    Add database (SQLite)

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express'); //package for make node as web server
 var app = express(); //instance for express package
 var ejs = require('ejs'); //package for view engine
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
 
 app.set('view engine', 'ejs'); // set the view engine to ejs
 
+// page render
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
+
+// API
+
 
 app.listen(8080);
 console.log("Server listening at Port 8080");
