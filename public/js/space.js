72efd36b85221f912c0bfc02e8e290fda545aae3
sthira <ss.mailone@gmail.com>
2017-06-11 05:29:48 +0700

    update#5

--- public/js/space.js
+++ public/js/space.js
@@ -1,40 +1,30 @@
-$(".space-btn").click(function(){
-    $.ajax({
-        method: "POST",
-        url: "../save.php",
-        data: { 
-            action: "hit-space-book-but", 
-            title: "-----", 
-            need: "-----", 
-            location: 0, 
-            name: "-----", 
-            phone: "-----", 
-            email: "-----", 
-            space: $("input[name=space]").val() 
-        }
-    })
-});
 
-$(".buy-btn").click(function(){
-    $.ajax({
-        method: "POST",
-        url: "../save.php",
-        data: { 
-            action: "hit-space-buymodal-but", 
-            title: "-----", 
-            need: "-----", 
-            location: 0, 
-            name: $("input[name=name]").val(), 
-            phone: $("input[name=phone]").val(),
-            email: $("input[name=email]").val(),
-            space: $("input[name=space]").val() 
-        }
-    })
-});
+var sum_factor = 1;
+var base_price = $(".space-price-amount").attr("data-value");
+var total_price = base_price;
+var hour = 1;
 
-$(".space-hour-input").on("change", function() {
-    $(".space-price-amount").html($(".space-price-amount").attr("data-value")*this.value);
-    if(this.value!=1)
-        $(".space-price-hour").html(this.value);
+var addon_price = 0;
+
+$(".space-book-input").on("change", function() {
+    sum_factor = parseInt($(".space-book-input").eq(0).val())*parseFloat($(".space-book-input").eq(1).val(),10);  // Or this.innerHTML, this.innerText
+    hour = $(".space-book-input").eq(0).val();
+    if(hour!=1)
+        $(".space-price-hour").html(hour);
     else $(".space-price-hour").html("");
+    update_price();
 })
+
+
+$("input[name='addon-checkbox']").on("change", function() {
+    addon_price = 0;
+    $("input[name='addon-checkbox']:checked").each(function(){
+        addon_price += parseInt($(this).val(),10);  // Or this.innerHTML, this.innerText
+    });
+    update_price();
+})
+
+function update_price(){
+    total_price = (base_price*sum_factor) + addon_price;
+    $(".space-price-amount").html(total_price);
+}
